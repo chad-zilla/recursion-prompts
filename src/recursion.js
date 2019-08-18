7e0812b63fc5957b4615d43428a4008fa38a4c9c
Chad Mueller <chad@websitesbeyond.com>
2019-08-17 19:13:48 -0700

    Complete factorial, sum, arraySum, isEven, and sumBelow

--- src/recursion.js
+++ src/recursion.js
@@ -7,26 +7,69 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if( n < 0 ){
+		return null;
+	}
+
+	if( n === 0 ){
+		return 1;
+	}
+
+	return n * factorial(  (n-1) );
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+	if( array.length < 1 ){
+		return 0;
+	} else {
+		return array[0] + sum(array.slice(1));	
+	}
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+	let currentValue = array[0];
+
+	if( Array.isArray(array[0]) ){
+		currentValue = arraySum(array[0]);
+	}
+
+	if( array.length < 1 ){
+		return 0;
+	} else {
+		return currentValue + arraySum(array.slice(1));	
+	}
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	n = Math.abs(n);
+	if( n === 0 ){
+		return true;
+	} else if (n === 1) {
+		return false;
+	} else {
+		return isEven( n - 2 );
+	}
 };
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+	if( n === 0 ){
+		return 0;
+	}
+
+	if( n < 0 ){
+		return (n+1) + sumBelow(n+1);
+	} else {
+		return (n-1) + sumBelow(n-1);
+	}
+	
 };
 
 // 6. Get the integers within a range (x, y).
